355b5e64707295643cf5a2855fa927df0f866e52
uwizeyejtag <uwizeyej@techaffinity.com>
2022-02-21 10:00:15 +0200

    first commit

---
+++ css/test.js
@@ -0,0 +1,12 @@
+var is=[1,2,3,4,5,6];
+var sum=[];
+var number;
+for (var a=0;a<=is.length-1;a++){
+  for (var b=is.length-1;b>=a;b--){
+    number=0;
+    // number = number + is[b];
+    console.log(is[b])
+  }
+  sum[a]=number;
+  // console.log(sum);
+}
